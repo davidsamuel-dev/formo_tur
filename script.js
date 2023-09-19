0b82d953913de9299245c854e1a737e8d128239b
davidsamuel-dev <davidsamuel.dev@gmail.com>
2023-09-19 13:56:51 -0300

    ENVIO DO MENU

---
+++ script.js
@@ -0,0 +1,19 @@
+var caixa_menu = document.getElementsByTagName("aside")[0]
+
+
+
+function menu() {
+ 
+    caixa_menu.style.top = "0"
+    // caixa_menu.style.width = "100%"
+    caixa_menu.style.display = "inline-block"
+
+}
+
+function voltar_menu() {
+
+   
+    caixa_menu.style.top = "-410px";
+    // caixa_menu.style.width = "300px";;
+    
+}
