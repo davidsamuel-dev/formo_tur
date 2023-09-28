80ee0c68dca3c2d1977d850a78d172d22effaeb3
rany04 <iorrany0430@gmail.com>
2023-09-28 17:11:05 -0300

    iorrany03

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ function voltar_menu() {
     // caixa_menu.style.width = "300px";;
     
 }
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+  var ul = document.querySelector('nav ul');
+  var menuBtn = document.querySelector('.menu-btn i');
