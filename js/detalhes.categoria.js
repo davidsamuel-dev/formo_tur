e28ebb26f121f4bba543e772a7e012353e620d92
Luis Carlos - [front] <luisestrela.bol951@gmail.com>
2023-10-16 20:52:54 -0300

    adiciona pagina de detalhes

---
+++ js/detalhes.categoria.js
@@ -0,0 +1,34 @@
+
+// url da index
+const GOBACK_PATH = '/index.html'
+
+// id dos elementos usados na pagina
+const GOBACK_BUTTON_ID = '#go-back'
+const HEART_ID = '#heart'
+const FAVORITE_BUTTON = '#favorite-item'
+
+// elementos do HTML
+const goBack = document.querySelector(GOBACK_BUTTON_ID)
+const heart = document.querySelector(HEART_ID)
+const favoriteItem = document.querySelector(FAVORITE_BUTTON)
+
+function favoritaCategoria() {
+  const tipoAtual = heart.getAttribute('type')
+
+  if(tipoAtual === 'regular') {
+    heart.setAttribute('type', 'solid')
+    heart.setAttribute('color', '#fc2803')
+    return;
+  }
+  heart.setAttribute('type', 'regular')
+  heart.setAttribute('color', '#009092')
+
+}
+
+function voltarPraIndex() {
+  localStorage.setItem('titulo', JSON.stringify('titulo da pagina'))
+  window.location.href = GOBACK_PATH
+}
+
+goBack.addEventListener('click', voltarPraIndex)
+favoriteItem.addEventListener('click', favoritaCategoria)
